62ed0617ead09e0d64300227cbd0baf984f8c332
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-09-29 13:35:02 +0600

    homepage initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_1/homepage.dart';
+import 'homepage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material( 
-        
-        child: Center(
-          child: Container(
-            child: Text("Welcome to App Dev with Flutter!"),
-      ),
-        )
-        ),
+      home: HomePage(),
     );
   }
 }
