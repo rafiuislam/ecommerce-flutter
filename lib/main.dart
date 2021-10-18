583df5719d7f578efa9845edaf806af0663573be
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-10-18 22:18:48 +0600

    google font and login page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_1/pages/homepage.dart';
 import 'package:flutter_app_1/pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/homepage.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -18,9 +20,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.cyan),
       darkTheme: ThemeData(
         primarySwatch: Colors.brown,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        // primaryTextTheme: GoogleFonts.latoTextTheme()
         // brightness: Brightness.dark,
         ),
-        initialRoute: "/home",
+        initialRoute: "/",
         routes: {
           "/": (context) => LoginPage(),
           "/home": (context) => HomePage(),
