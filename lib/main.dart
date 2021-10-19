0de7585c7e7a092faead07373da95a7ced92aeaa
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-10-19 20:16:35 +0600

    Navigation from login to homepage added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_1/pages/homepage.dart';
 import 'package:flutter_app_1/pages/loginpage.dart';
+import 'package:flutter_app_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/homepage.dart';
 
@@ -24,11 +25,12 @@ class MyApp extends StatelessWidget {
         // primaryTextTheme: GoogleFonts.latoTextTheme()
         // brightness: Brightness.dark,
         ),
+        debugShowCheckedModeBanner: false,
         initialRoute: "/",
         routes: {
           "/": (context) => LoginPage(),
-          "/home": (context) => HomePage(),
-          "/login": (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => HomePage(),
+          MyRoutes.loginRoute: (context) => LoginPage(),
         },
     );
   }
