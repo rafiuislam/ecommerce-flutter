060154f2cea5f59742b8b54ac0be3c2c6f90b98e
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-10-24 20:39:06 +0600

    ListView Builder and Card to veiw items on app

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_1/models/catalog.dart';
 import 'package:flutter_app_1/widgets/drawer.dart';
+import 'package:flutter_app_1/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   @override
   Widget build(BuildContext context) {
+    final dummy = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "App Catagories",
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days App Dev with Flutter!"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummy.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummy[index],
+              );
+            }),
       ),
       drawer: MyDrawer(),
     );
