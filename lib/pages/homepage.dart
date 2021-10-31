2f10ba29784efc3b371e6723db2870245d387b98
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-10-31 10:28:45 +0600

    load and decode Json

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,32 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app_1/models/catalog.dart';
 import 'package:flutter_app_1/widgets/drawer.dart';
 import 'package:flutter_app_1/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var prouctsData = decodedData["products"];
+    print(prouctsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummy = List.generate(20, (index) => CatalogModel.items[0]);
