f8846b213948865c5d49aa9ad3e79480c9f4c56f
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-11-10 12:03:12 +0600

    building basic outline for cartPage

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -9,10 +9,65 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: context.canvasColor,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: "Cart".text.color(context.theme.accentColor).make(),
+        backgroundColor: context.canvasColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: "Cart".text.color(context.theme.accentColor).make(),
+        ),
+        body: Column(
+          children: [
+            _CartList().p32().expand(),
+            const Divider(),
+            const _CartTotal(),
+          ],
+        ));
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          "\$300".text.xl4.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(context.theme.buttonColor),
+                shape: MaterialStateProperty.all(const StadiumBorder())),
+            child: "Buy".text.make(),
+          ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  __CartListState createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 7,
+      itemBuilder: (context, index) => ListTile(
+        title: "Item".text.make(),
+        subtitle: "Subtitle".text.make(),
+        leading: const Icon(Icons.done),
+        trailing: IconButton(
+          icon: const Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
       ),
     );
   }
